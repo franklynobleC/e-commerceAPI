3ba8b82853b2bac78701fade09d91c4627f4c6d6
franklynobleC <franklyessien01@gmail.com>
2023-02-17 03:21:41 -0500

    add db connection url

--- starter/app.js
+++ starter/app.js
@@ -1,44 +1,34 @@
-require('dotenv').config()  // get  env file 
+const express = require('express')
+require('dotenv').config()
+const DbConnection = require('./db/connectdb');
 
-const express = require('express');
+ 
+
+
 const app = express();
-//async errors 
-require('express-async-errors');
 
-const stripeController = require('./controllers/stripeController')
- //error handler
- const notFoundMiddleware = require('./middleware/error-handlers');
+ const port = process.env.PORT || 4000;
+
 
    
- // make  all the  files  in this  publicy available
-//getting data  in json
-app.use(express.json())
-// app.use(fileUpload());
-
-
-
-
-//routes 
-
-
-app.post('/stripe', stripeController)
-
-//if error, use  this route 
-app.use(notFoundMiddleware)
-// app.use(errorMiddleware)
-
-const port = process.env.PORT  || 3000  // if the  port is  undefined, use  port 3000
-
-const start = async() => {
+ const  start = async() => {
+    
     try {
-   
-
-        app.listen(port, console.log(`Server is  listening ${port}...`))
-        console.log("from  main")
-        // console.log(cloudinary.config.process.env.CLOUD_API_KEY)
+ const db = await  DbConnection(process.env.MONGO_URI);
+  console.log('connection successful', db)
     }catch(err) {
-        console.log(err.message, 'error Message')
-    }
-}
-start();
 
+ console.log('can not connect to DB !!', err)  
+    }
+
+
+    try {
+app.listen(port, console.log('Server is  listening', `${port}`))
+
+    }catch (err) {
+        console.log('could  not connetc err', err)
+
+    }    
+ };
+
+ start();
